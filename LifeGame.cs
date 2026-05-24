5c98df16539c089420746fbcb7390b600187b571
adirida102-pixel <adirida102@gmail.com>
2026-05-24 10:44:18 +0300

    life game save

---
+++ LifeGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdirEf10th
+{
+    public class LifeGame
+    {
+        private int[,] matrix;
+        
+        public static void LifeGame_UT()
+        {
+            //pass
+        }
+
+        public LifeGame(int[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public int[,] GetMatrix()
+        {
+            return this.matrix;
+        }
+
+        public void SetMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    this.matrix[row, col] = matrix[row, col];
+                }
+            }
+        }
+
+        public int[] GetNeighbors()
+        {
+            //pass
+        }
+
+        public void NextGeneration()
+        {
+            //pass
+        }
+
+        public void PrintBoard()
+        {
+            //pass
+        }
+    }
+}
